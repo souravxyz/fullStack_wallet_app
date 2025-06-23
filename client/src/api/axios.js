7761e31987a1e0fe3f423ac689a8d3cf989d6270
souravxyz <srivastavasourav68689@gmail.com>
2025-06-24 00:00:39 +0530

    a

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
